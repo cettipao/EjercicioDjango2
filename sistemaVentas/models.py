9127453419672611105485b8ff4765bbb9e60c16
cettipao <cettipao@live.com.ar>
2020-06-25 12:00:18 -0300

    Clase Cateoria, Cliente y Telefono agregadas

--- sistemaVentas/models.py
+++ sistemaVentas/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class Categoria(models.Model):
+    nombre = models.CharField(max_length = 20)
+    descripcion = models.CharField(max_length = 40)
+
+class Cliente(models.Model):
+    nombre = models.CharField(max_length = 20)
+    telefonos = models.ForeignKey(
+        'Telefono',
+        on_delete=models.CASCADE,
+        null=False
+    )
+
+class Telefono(models.model):
+    numero = models.CharField(max_length = 20)
+    cliente = models.ForeignKey(
+        'Cliente',
+        on_delete=models.CASCADE,
+        null=False
+    )
